d3da16cf42201e0053ec0e3f31a15c763c0d84e5
“Jamie <knutsenjamie@yahoo.com>
2021-05-07 13:22:37 -0700

    refactor first test again

--- BakeryApplication/Models/BreadClass.cs
+++ BakeryApplication/Models/BreadClass.cs
@@ -2,12 +2,7 @@ namespace BakeryApplication.Models
 {
     public class BreadClass
     {
-        public int BreadPrice { get; set; }  
-        
-        public int Bread ()
-        {
-        return 4;
-        }
+     
     }   
 }
 
