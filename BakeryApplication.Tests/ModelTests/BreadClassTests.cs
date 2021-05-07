92683a55a1180a28ff3a4f357560839b42e5040f
“Jamie <knutsenjamie@yahoo.com>
2021-05-07 11:16:50 -0700

    WIP: clear all tests and class methods for better testing

--- BakeryApplication.Tests/ModelTests/BreadClassTests.cs
+++ BakeryApplication.Tests/ModelTests/BreadClassTests.cs
@@ -7,17 +7,6 @@ namespace BakeryApplication.Tests
     public class BreadClassTests
     {
         [TestMethod]
-        public void GetAmount_ReturnAmount_int()
-        {
-        BreadClass breadOrder = new BreadClass();
-        Assert.AreEqual(5, breadOrder.BreadPrice(1));
-        }
-        [TestMethod]
-        public void GetAmountSecond_ReturnAmountSecond_int()
-        {
-        BreadClass breadOrder = new BreadClass();
-        Assert.AreEqual(60, breadOrder.BreadPrice(12));
-        }
         
     }
 }
