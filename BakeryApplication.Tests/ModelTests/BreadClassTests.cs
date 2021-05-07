5152f588420d69a75e418782068d4fb7e0b41ce0
“Jamie <knutsenjamie@yahoo.com>
2021-05-07 13:50:12 -0700

    WIP: Attempt second test with no success

--- BakeryApplication.Tests/ModelTests/BreadClassTests.cs
+++ BakeryApplication.Tests/ModelTests/BreadClassTests.cs
@@ -7,11 +7,19 @@ namespace BakeryApplication.Tests
     public class BreadClassTests
     {
         [TestMethod]
-        public void Bread_InstantiateEmptyBreadConstructor_Bread()
+        public void BreadPrice_InstantiateEmptyBreadPriceConstructor_BreadPrice()
         {
         BreadClass newBread = new BreadClass ();
         Assert.AreEqual(typeof(BreadClass), newBread.GetType());
         }
+        [TestMethod]
+        public void GetBreadAmount_ReturnPrice_Int()
+        {
+        BreadClass newBread = new BreadClass (1); 
+        Assert.AreEqual(1, newBread.BreadPrice(5.00));  
+        }
+
+    
         
     }
 }
--- BakeryApplication/Models/BreadClass.cs
+++ BakeryApplication/Models/BreadClass.cs
@@ -2,7 +2,13 @@ namespace BakeryApplication.Models
 {
     public class BreadClass
     {
-       
+        public int BreadAmount { get; set;}
+
+        public BreadPrice(int breadAmount)
+        {
+            BreadAmount = breadAmount;
+            return BreadAmount * 5.00;
+        }
     }   
 }
 
