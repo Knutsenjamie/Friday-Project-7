3eaca3e6e1c7b12943535b0184442ec697aa1236
“Jamie <knutsenjamie@yahoo.com>
2021-05-16 13:07:45 -0700

    refactor for better syntax

--- BakeryApplication/Program.cs
+++ BakeryApplication/Program.cs
@@ -14,14 +14,14 @@ namespace BakeryApplication
             int personsInput = int.Parse(personInput);
 
             BreadClass breadOrder = new BreadClass();
-            Console.WriteLine($"\n Price: ${breadOrder.BreadCount(personsInput)} & you also get { breadOrder.BreadTotal()} for free! \n");
+            Console.WriteLine($"\n Price: ${breadOrder.BreadCount(personsInput)} but you also get { breadOrder.BreadTotal()} of those for free! \n");
 
             Console.WriteLine("How many pastries would you like? Please enter in a number to see your total price: \n");
             string result1 = Console.ReadLine(); 
             int result2 =  int.Parse(result1);
 
 
-            Console.WriteLine($"\n The pastries cost will be: ${PastryClass.pastryPrice(result2)} \n");
+            Console.WriteLine($"\n The pastries cost will be: ${PastryClass.pastryPrice(result2)} \n Since you also get the 3 for $5 deal!");
             int total = breadOrder.BreadCount(personsInput) + PastryClass.pastryPrice(result2);
             Console.WriteLine("\n For your amount of baked goods, your total is: $" + total + ". Have a wonderful day! \n");
         }
