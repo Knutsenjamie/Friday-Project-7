3d42215529ffd03849148a9cc576019da3dc21e1
“Jamie <knutsenjamie@yahoo.com>
2021-05-18 09:10:56 -0700

    add second BreadClass test for GetBreadLoafAmount and pass test

--- BakeryApplication.Tests/ModelTests/BreadClassTests.cs
+++ BakeryApplication.Tests/ModelTests/BreadClassTests.cs
@@ -9,8 +9,16 @@ namespace BakeryApplication.Tests
         [TestMethod]
         public void BreadClassConstructor_InstantiateEmptyBreadClassConstructor_BreadClass()
         {
-        BreadClass newBread = new BreadClass(5);
-        Assert.AreEqual(typeof(BreadClass), newBread.GetType());
+            BreadClass newBread = new BreadClass(5);
+            Assert.AreEqual(typeof(BreadClass), newBread.GetType());
+        }
+        [TestMethod]
+        public void GetBreadLoafAmount_ReturnsBreadLoafAmount_BreadClass()
+        {
+            int breadLoafAmount = 5;
+            BreadClass newBread = new BreadClass(5);
+            Assert.AreEqual(breadLoafAmount, newBread.BreadLoafAmount);
+
         }
         
     }
