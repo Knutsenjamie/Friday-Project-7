f426491fd942f358a4c609d23c9a84d3b4f4ef0b
“Jamie <knutsenjamie@yahoo.com>
2021-05-18 09:00:03 -0700

    finish BreadClass constructor and add empty calculation methods

--- BakeryApplication/Models/BreadClass.cs
+++ BakeryApplication/Models/BreadClass.cs
@@ -5,12 +5,24 @@ namespace BakeryApplication.Models
         public int BreadLoafAmount { get; set; }
         public int BreadCost { get; set; }
         public int BonusBreadLoaves { get; set; }
-        public int TotalBreadLoafCost { get; set; }
+        public int FinalBreadLoafCost { get; set; }
 
         public BreadClass(int breadLoafAmount)
         {
             BreadLoafAmount = breadLoafAmount;
             BreadCost = CalculatedBaseCost(breadLoafAmount);
+            BonusBreadLoaves = CalculatedBonusCost(breadLoafAmount);
+            FinalBreadLoafCost = BreadLoafAmount + BonusBreadLoaves;
+        }
+
+        public int CalculatedBaseCost(int breadLoafAmount)
+        {
+
+        }
+
+        public int CalculatedBonusCost(int breadLoafAmount)
+        {
+
         }
     }   
 }
